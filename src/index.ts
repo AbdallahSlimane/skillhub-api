943f85489e0859dd9208c09a9741a9c2c351024f
AbdallahSlimane <slimane.abdallah75@gmail.com>
2025-03-06 11:30:47 +0100

    add user, article, domain

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import "reflect-metadata";
+import express from "express";
+import { config } from "dotenv";
+import cors from 'cors';
+import {db, initialize_typeorm} from "./data-source/data-source";
+
+config();
+
+const PORT = process.env.APP_PORT || 3000;
+
+export async function startServer() {
+    try {
+        const app = express();
+        app.use(express.json());
+
+        app.use(cors());
+        // app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:3001'] }));
+
+        // app.use('/user', userRoutes);
+
+        await initialize_typeorm(db);
+        console.log("Database connected successfully");
+
+        app.listen(PORT, () => {
+            console.log(`Server started at http://localhost:${PORT}/`);
+        });
+
+    } catch (error) {
+        console.error("Error starting the server:", error);
+    }
+}
+
+startServer().catch((error) => console.error("Unexpected error:", error));
