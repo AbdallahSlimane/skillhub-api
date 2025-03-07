c70b9997ca3ba945bc8d4530c41087cf34f95386
AbdallahSlimane <slimane.abdallah75@gmail.com>
2025-03-07 10:19:48 +0100

    fix chatbot entity

--- src/models/chatbot.ts
+++ src/models/chatbot.ts
@@ -1,6 +1,5 @@
 import {Column, Entity} from "typeorm";
 
-@Entity({ name: "chatbots" })  // Le nom de la table peut être "chatbots"
 export class Chatbot {
     @Column("text", { name: "message" })
     message: string;
