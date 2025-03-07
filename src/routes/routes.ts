05487fef81fdd828451d3961dce38bcde53823e8
Karl OELSCHLAGER <oelschlager.karl@gmail.com>
2025-03-07 10:53:29 +0100

    feat(add): add short_articles & routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,9 +5,12 @@ import { VoteController } from "../controllers/vote.controller";
 import { ArticleService } from "../services/article.service";
 import { UserService } from "../services/user.service";
 import { VoteService } from "../services/vote.service";
+import { ShortArticleService } from "../services/short-article.service";
+import { ShortArticleController } from "../controllers/short-article.controller";
 import { Article } from "../models/article";
 import { User } from "../models/user";
 import { Vote } from "../models/vote";
+import { ShortArticle } from "../models/short-article";
 import { db } from "../data-source/data-source";
 import {Chatbot} from "../models/chatbot";
 import {ChatbotService} from "../services/chatbot.service";
@@ -19,6 +22,7 @@ const articleRepository = db.getRepository(Article);
 const userRepository = db.getRepository(User);
 const voteRepository = db.getRepository(Vote);
 const chatbotRepository = db.getRepository(Chatbot);
+const shortArticleRepository = db.getRepository(ShortArticle);
 
 
 
@@ -26,18 +30,21 @@ const articleService = new ArticleService(articleRepository);
 const userService = new UserService(userRepository);
 const voteService = new VoteService(voteRepository, articleRepository);
 const chatbotService = new ChatbotService(chatbotRepository);
+const shortArticleService = new ShortArticleService(shortArticleRepository);
 
 
 const articleController = new ArticleController(articleService, userService);
 const userController = new UserController(userService);
 const voteController = new VoteController(voteService);
 const chatbotController = new ChatbotController(chatbotService);
+const shortArticleController = new ShortArticleController(shortArticleService);
 
 
 const userRouter = Router();
 const articleRouter = Router();
 const voteRouter = Router();
 const chatbotRouter = Router();
+const shortArticleRouter = Router();
 
 userRouter.post("/register", (req, res) => userController.register(req, res));
 userRouter.post("/login", (req, res) => userController.login(req, res));
@@ -53,7 +60,8 @@ articleRouter.get("/:id", (req, res) => articleController.getArticle(req, res));
 articleRouter.put("/update/:id", (req, res) => articleController.updateArticle(req, res));
 articleRouter.delete("/delete/:id", (req, res) => articleController.deleteArticle(req, res));
 
-
+shortArticleRouter.post("/create", (req, res) => shortArticleController.postShortArticle(req, res));
+shortArticleRouter.get("/:article_id", (req, res) => shortArticleController.getShortArticle(req, res));
 
 voteRouter.post("/upvote", (req, res) => voteController.upvote(req, res));
 voteRouter.post("/downvote", (req, res) => voteController.downvote(req, res));
@@ -61,4 +69,4 @@ voteRouter.get("/:articleId", (req, res) => voteController.getVotesByArticle(req
 
 chatbotRouter.post("/chatbot", (req, res) => chatbotController.createMessage(req, res));
 
-export { userRouter, articleRouter, voteRouter, chatbotRouter };
+export { userRouter, articleRouter, voteRouter, chatbotRouter, shortArticleRouter };
